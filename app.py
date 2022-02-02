8c347edbcf81e4af05c77a4e0c7547972928b118
al-bratkov <al.bratkov15@gmail.com>
2022-02-02 22:30:40 +0300

    переделал шаблоны url под query strings

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -16,11 +16,13 @@ def todo_create():
     return str(to_do_list)
 
 
-@app.route('/add/<task>')
-def todo_add(task):
+@app.route('/add')
+def todo_add():
     """Создание новой задачи
     реализовал через словарь
     пока в нем номер позиции (индекс, начиная с 1) и название"""
+#    here we want to get the value of user (i.e. ?task=some-value)
+    task = request.args.get('task')
     note = {'pos': len(to_do_list)+1, 'title': task}
     to_do_list.append(note)
     print(to_do_list)
@@ -32,18 +34,21 @@ def todo_read():
     return str(to_do_list)
 
 
-@app.route('/update/<position>/<new_task>')
-def todo_update(position, new_task):
+@app.route('/update')
+def todo_update():
     """Передается не индекс, а номер позиции ТуДуЛиста
      (начинается не с 0, а с 1)"""
+    new_task = request.args.get('new_task')
+    position = request.args.get('position')
     to_do_list[int(position)-1]['title'] = new_task
     return str(to_do_list)
 
 
-@app.route('/delete/<position>')
-def delete(position):
+@app.route('/delete')
+def delete():
     """Передается не индекс, а номер позиции ТуДуЛиста
     (начинается не с 0, а с 1)"""
+    position = request.args.get('position')
     not_finished = to_do_list.pop(int(position)-1)
     for i in range(int(position)-1, len(to_do_list)):
         to_do_list[i]['pos'] = i+1
@@ -52,3 +57,4 @@ def delete(position):
 
 if __name__ == '__main__':
     app.run()
+
